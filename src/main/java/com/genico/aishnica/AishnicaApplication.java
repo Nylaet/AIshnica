f9229641236e14d3a5a054ba4b2b1132954bf2ba
Dmitriy Lomko <nylaet@gmail.com>
2025-02-26 14:36:58 +0200

    stage

---
+++ src/main/java/com/genico/aishnica/AishnicaApplication.java
@@ -0,0 +1,12 @@
+package com.genico.aishnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AishnicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AishnicaApplication.class, args);
+    }
+}
